2933a71741e96635f13c530a88d3a9cb726942e5
Carlos Lopez <Carlos Lopez@DESKTOP-HQ482UQ>
2024-03-19 00:01:21 -0600

    Agregar archivos de proyecto.

---
+++ UdeM_Reportes.Module/BusinessObjects/Solicitudes.cs
@@ -0,0 +1,68 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace UdeM_Reportes.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+
+    public class Solicitudes : BaseObject
+    {
+
+        public Solicitudes(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            FechaRegistro = DateTime.Now;
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+        public enum EstadoSolicitud
+        {
+            Creado = 0,
+            [XafDisplayName("Publicado")]
+            Publicado = 1,
+            EnRevision = 2,
+            Entregado = 3,
+            Rechazado = 4
+        }
+
+        Persona persona;
+        CatalogoUbicacion ubicacionDePeticion;
+        CatalogoProfesores profesor;
+        CatalogoCarrera carrera;
+        DateTime fechaRegistro;
+        EstadoSolicitud estado;
+        string descripcion;
+
+
+        public DateTime FechaRegistro { get => fechaRegistro; set => SetPropertyValue(nameof(FechaRegistro), ref fechaRegistro, value); }
+
+        public EstadoSolicitud Estado { get => estado; set => SetPropertyValue(nameof(Estado), ref estado, value); }
+        [Size(200)]
+        public string Descripcion { get => descripcion; set => SetPropertyValue(nameof(Descripcion), ref descripcion, value); }
+
+        public CatalogoCarrera Carrera { get => carrera; set => SetPropertyValue(nameof(Carrera), ref carrera, value); }
+
+        public CatalogoProfesores Profesor { get => profesor; set => SetPropertyValue(nameof(Profesor), ref profesor, value); }
+
+        public CatalogoUbicacion UbicacionDePeticion { get => ubicacionDePeticion; set => SetPropertyValue(nameof(UbicacionDePeticion), ref ubicacionDePeticion, value); }
+
+        [Association("Persona-Solicitudes")]
+        public Persona Persona { get => persona; set => SetPropertyValue(nameof(Persona), ref persona, value); }
+
+    }
+}
---
+++ UdeM_Reportes.Module/BusinessObjects/Usuario.cs
@@ -0,0 +1,65 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace UdeM_Reportes.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+
+    public class Usuario : BaseObject
+    {
+        public Usuario(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            FechaRegistro = DateTime.Now;
+        }
+
+
+        bool usuarioExiste;
+        EstadoUsuario estado;
+        string passwordAgain;
+        string username;
+        string password;
+        DateTime fechaRegistro;
+
+        public DateTime FechaRegistro { get => fechaRegistro; set => SetPropertyValue(nameof(FechaRegistro), ref fechaRegistro, value); }
+
+        public enum EstadoUsuario
+        {
+            Activo = 1,
+            Inactivo = 0
+        }
+
+        [Size(50)]
+        public string Username { get => username; set => SetPropertyValue(nameof(Username), ref username, value); }
+
+        [NonPersistent]
+        public bool UsuarioExiste { get => usuarioExiste; set => SetPropertyValue(nameof(UsuarioExiste), ref usuarioExiste, value); }
+
+        [Size(100)]
+        public string Password { get => password; set => SetPropertyValue(nameof(Password), ref password, value); }
+
+        [NonPersistent]
+        [Size(100)]
+        public string PasswordAgain { get => passwordAgain; set => SetPropertyValue(nameof(PasswordAgain), ref passwordAgain, value); }
+
+        public EstadoUsuario Estado { get => estado; set => SetPropertyValue(nameof(Estado), ref estado, value); }
+
+        [Association("Usuario-Persona")]
+        public XPCollection<Persona> Persona => GetCollection<Persona>(nameof(Persona));
+    }
+}
